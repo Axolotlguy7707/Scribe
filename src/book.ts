2f29be444249a97030d6852ccd26b7610506cd84
Axolotlguy <practicleant1321@outlook.com>
2026-03-28 16:21:12 -0500

    idk

--- src/book.ts
+++ src/book.ts
@@ -1,19 +1,54 @@
 ﻿// File used for pretty much everything a typical Scribe project would need.
 
 // Metadata
-let bookTitle: string;
-let publisher: string;
-let author: string;
-let chapters: Chapter[] = [];
+let bookTitle: string = "";
+let publisher: string = "";
+let author: string = "";
 
+// Chapter type
 export type Chapter = {
     title: string;
     content: string;
 };
 
+// In-memory chapter list
+let chapters: Chapter[] = [];
 
-// Getters
-export function getMetadata()
-{
-    return {chapters: chapters, publisher: publisher, author: author, bookTitle: bookTitle};
+// Index of the currently open chapter
+let currentChapterIndex: number = -1;
+
+// --- Getters ---
+export function getMetadata() {
+    return { chapters, publisher, author, bookTitle };
+}
+
+export function getChapters() {
+    return chapters;
+}
+
+export function getCurrentChapter() {
+    return chapters[currentChapterIndex];
+}
+
+export function getCurrentChapterIndex() {
+    return currentChapterIndex;
+}
+
+// --- Setters ---
+export function addChapter(title: string) {
+    chapters.push({ title, content: "" });
+}
+
+export function setCurrentChapter(index: number) {
+    currentChapterIndex = index;
+}
+
+export function updateCurrentChapterContent(content: string) {
+    if (currentChapterIndex >= 0) {
+        chapters[currentChapterIndex].content = content;
+    }
+}
+
+export function saveCurrentChapter(chapter: Chapter) {
+    // TODO, add chapter saving
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import {clearEditorText, initEditor, setEditorText} from "./editor";
+import { addChapter } from "./book.ts";
 
 let newButton: any = document.getElementById("new");
+
+let newChapButton: any = document.getElementById("newChapter");
 
 initEditor();
 
@@ -8,4 +11,8 @@ setEditorText("Hello World!");
 
 newButton.addEventListener("click", () => {
     clearEditorText();
+})
+
+newChapButton.addEventListener("click", () => {
+    addChapter("Title");
 })
